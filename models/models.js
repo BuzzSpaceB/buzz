b4c0213d8db1ba94464b2984217d4e0af8622401
AnrichVS <AnrichVS@gmail.com>
2015-03-21 16:35:05 +0200

    Started working on models (Mongoose Schemas) for our MongoDB database.

---
+++ models/models.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Anrich on 3/21/2015.
+ */
+
+/* See http://stackoverflow.com/questions/10081611/mongoose-schema-creation */
+
+var models = ['users.js'];
+
+exports.initialize = function()
+{
+    var len = models.length;
+    for(var i = 0; i < len; ++i)
+    {
+        require(models[i])();
+    }
+};
---
+++ models/users.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Anrich on 3/21/2015.
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = function()
+{
+    var users = new Schema({
+        user_id     : String,
+        profile_pic : { data: Buffer, contentType: String }, /* See https://gist.github.com/aheckmann/2408370 */
+        preferred_email : String
+    });
+    mongoose.model("users", users);
+};
