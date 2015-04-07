a88d6b89b8a68725e1dfbb5484cb168a939634e5
Thinus Naude <thinusn@gmail.com>
2015-04-07 10:41:36 +0200

    :)

--- models/notification.js
+++ models/notification.js
@@ -1,20 +1,13 @@
-/**
- * Created by Anrich on 3/22/2015.
- */
-
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-module.exports = function()
-{
-    var Notification = new Schema({
-        notification_id             : String,            /* PK */
-        thread_id                   : String,           /* Notifications relate to a specific thread */
-        user_id                     : String,           /* A notification will be sent to a specific user */
-        date_time                   : Date,             /* A notification will show its date and time */
-        type                        : String,           /* Each notification has a type, like Delete, New Post etc. */
-        content                     : String,           /* The actual notification text */
-        read                        : Boolean           /* Flag to show notification has been read */
-    });
-    mongoose.model("Notification", Notification);
-};
+var NotificationSchema = mongoose.Schema({
+    notification_id             : String,            /* PK */
+    thread_id                   : String,           /* Notifications relate to a specific thread */
+    user_id                     : String,           /* A notification will be sent to a specific user */
+    date_time                   : Date,             /* A notification will show its date and time */
+    type                        : String,           /* Each notification has a type, like Delete, New Post etc. */
+    content                     : String,           /* The actual notification text */
+    read                        : Boolean           /* Flag to show notification has been read */
+});
+
+module.export = mongoose.model("notifications", NotificationSchema);
