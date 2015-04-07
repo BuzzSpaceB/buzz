8cdb198988237c2120fd8f64fc29d21161125f04
AnrichVS <AnrichVS@gmail.com>
2015-04-07 12:58:58 +0200

    Threads for some reason created their own mongoose Schemas, after schemas were provided to them. I added their schemas to models.

--- models/thread.js
+++ models/thread.js
@@ -1,14 +1,13 @@
 var mongoose = require('mongoose');
 
 var ThreadSchema = new mongoose.Schema({
-    thread_id       : String, 					/*PK*/
-    parent_thread_id: String,				    /*FK, Parent of the thread*/
-    user_id         : String,				    /*FK, Each Thread has one author*/
-    num_children    : Number,                   /* The number of children the Thread has */
-    closed          : Boolean,                  /* Flag to show thread is closed */
-    hidden          : Boolean,                  /* Flag to show thread is hidden */
-    level           : Number,                    /* Shows on which level the thread is currently at */
-    post_id         : String                    /* The post that is connected to the thread */
+    ID: mongoose.Schema.Types.ObjectId,
+    User: String,
+    Parent: mongoose.Schema.Types.ObjectId,
+    Level: Number,
+    Post: mongoose.Schema.Types.ObjectId,
+    Status: mongoose.Schema.Types.ObjectId,
+    Children: [mongoose.Schema.Types.ObjectId]
 });
 
 module.exports = mongoose.model("threads", ThreadSchema);
