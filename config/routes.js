4110ed2d95306aadd44864fe25065f616eb9cccc
TieniePritchard <TieniePritchard@outlook.com>
2015-04-10 16:31:31 +0200

    Changed Integration of Threads (NOTE: STILL EMULATING THREADS BEHAVIOUR - NOT ACTUAL INTEGRATION)

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,162 @@
-var User = require('../models/user');
+
+/*POSTING*/
+//This is what the functional teams had to write
+function postThreadToDatabase(JSONDetails)
+{
+    //Set up the schema
+    var Thread = require('../models/thread');
+    //Create The Thread
+    var newThread = new Thread();
+    newThread.parent_thread_id = JSONDetails.ParentID;
+    newThread.user_id = JSONDetails.UserID;
+    newThread.num_children = JSONDetails.NumChildren;
+    newThread.closed = JSONDetails.Closed;
+    newThread.hidden = JSONDetails.Hidden;
+    newThread.level = JSONDetails.Level;
+    newThread.post_id = JSONDetails.Post;
+    newThread.subject = JSONDetails.Subject;
+    newThread.module_id = JSONDetails.Module;
+    //This is how you save it
+    newThread.save(function (err) {
+        if (err) console.log("ERR: " + err);
+        console.log("Saving: " + JSON.stringify(newThread));
+    });
+}
+
+//This is what the functional integration teams had to write
+function createNewThread(subject, post, parentThread, module)
+{
+
+    var Thread = require('../models/thread');
+    //Find the level of the thread whose object_id is the same as the parent thread's ID (i.e. find the parent's level)
+    if(parentThread == null)
+    {
+        var level = 1;
+        //Construct a new Thread
+        var newThreadJSON = {"ParentID" : null, "UserID" : "u12345678", "NumChildren" : 0, "Closed" : false, "Hidden" : false, "Level" : level, "Post" : post, "Subject" : subject, "Module" : module};
+        //Post it to the Database
+        postThreadToDatabase(newThreadJSON);
+    }
+    else
+    {
+        Thread.findOne({ '_id' : parentThread }, function(err, parentThread)
+        {
+            if (err)
+                console.log("ERR: " + err);
+            else {
+                //Set the child thread's level
+                var parentLevel = parentThread.level;
+                var parentID = parentThread._id;
+                var level = 0;
+                level = Number(parentLevel) + 1;
+                //Construct a new Thread
+                var newThreadJSON = {"ParentID" : parentID, "UserID" : "u12345678", "NumChildren" : 0, "Closed" : false, "Hidden" : false, "Level" : level, "Post" : post, "Subject" : subject, "Module" : module};
+                //Post it to the Database
+                postThreadToDatabase(newThreadJSON);
+            }
+        });
+    }
+
+}
+
+/*GETTING A POST*/
+/*This is what the functional teams had to write*/
+function getThreadFromDatabase(module, parent, req, res, getThreadCallback)
+{
+    //Generate Threads from Database
+    var Thread = require('../models/thread');
+    //If no parent just find all the level 1 threads
+    //Find the parent thread
+    Thread.findOne({'_id': parent}, function (err, validThread)
+    {
+        if (err)
+            console.log("ERR: " + err);
+        else
+        {
+            //Create a JSON object consisting of that threads and return.
+            var stringifiedJSON = JSON.stringify(validThread);
+            var obj = JSON.parse(stringifiedJSON);
+            getThreadCallback(obj,parent, req, res);
+        }
+    });
+
+}
+/*This is what the functional teams had to write*/
+function generateThreads(validThread)
+{
+    console.log("BBB");
+    //Generate those threads
+    var threads = new Array();
+    var amount = Number(Object.keys(validThread).length);
+    var stringifiedJSON = JSON.stringify(validThread);
+    var obj = JSON.parse(stringifiedJSON);
+    for (var i = 0; i < amount; i++) {
+        threads[i] = {
+            heading: obj[i]["subject"],
+            name: obj[i]["user_id"],
+            level: obj[i]["level"],
+            date: "Tue Apr 07 2015 13:32PM",
+            post: obj[i]["post_id"],
+            spaceName: obj[i]["module_id"],
+            threadID: obj[i]["_id"],
+            userID: obj[i]["user_id"],
+            profilePick: "profile3.png",
+            moduleID: obj[i]["module_id"]
+        };
+
+    }
+    threads = JSON.stringify(threads);
+    threads = JSON.parse(threads);
+    return threads;
+}
+
+/*This is what the functional integration teams had to write*/
+function getChildThreads(module, parent, req, res, threadCreateCallback) {
+    console.log("AAAA");
+    var Thread = require('../models/thread');
+    //If no parent, just find all the level 1 threads
+    if (parent == null) {
+        //Find all the threads on level 1
+        Thread.find({'level': 1}, function (err, validThread) {
+            if (err)
+                console.log("ERR: " + err);
+            else {
+                var threads = generateThreads(validThread);
+                threadCreateCallback(req, res, threads);
+            }
+        });
+    }
+    else
+    {
+        getThreadFromDatabase(module, parent, req, res, getThreadCallback);
+    }
+}
+
+//Callback Functions to emulate Synchronous Behaviour
+function getThreadCallback(parentDetails, parent, req, res)
+{
+    var Thread = require('../models/thread');
+    var stringifiedJSON = JSON.stringify(parentDetails);
+    var obj = JSON.parse(stringifiedJSON);
+    var childLevel = Number(obj.level) + 1;
+    var module = obj.module_id;
+    //Find the threads on level of the thread for this module whose parent is the thread we navigated to
+    Thread.find({'level': childLevel, 'module_id': module, 'parent_thread_id': parent}, function (err, validThread) {
+        if (err)
+            console.log("ERR: " + err);
+        else {
+            var threads = generateThreads(validThread);
+            threadCreateCallback(req, res, threads);
+        }
+    });
+}
+
+function threadCreateCallback(req, res, threads)
+{
+    res.render('threads', {title: 'D3', threads: threads, user: req.user, content: 'This is the module code for a thread'});
+}
+
+
 //var users  = require('../models/UserModel');
 module.exports = function (router, passport) {
 
@@ -19,6 +177,7 @@ module.exports = function (router, passport) {
      * GET home page.
      */
     router.get('/t', function (req, res, next) {
+        var User = require('../models/user');
         var newStudent = new User();
         newStudent.username = "u12345678";
         newStudent.password = "password";
@@ -93,207 +252,64 @@ module.exports = function (router, passport) {
     });
 
     router.get('/spaces/:spaceName', isLoggedIn, function (req, res) {
-        //Generate Threads from Database
-        var amount = 0;
-        var threads = new Array();
-        var Thread = require('../models/thread');
 
-        var module = req.params.spaceName;
-        //Find the threads on level 1 for this module
-        Thread.find({'level' : "1", 'module_id' : module }, function (err, validThread)
+        try {
+            var module = req.params.spaceName;
+            var parent = null;
+            getChildThreads(module, parent, req, res, threadCreateCallback);
+        }
+        catch(e)
         {
+            console.log(e);
+        }
 
-                if (err)
-                    console.log("ERR: " + err);
-                else {
-                    //Generate those threads
-                    amount = Number(Object.keys(validThread).length);
-                    var stringifiedJSON = JSON.stringify(validThread);
-                    console.log(stringifiedJSON);
-                    var obj = JSON.parse(stringifiedJSON);
-                    for(var i = 0; i < amount; i++)
-                    {
-                        threads[i] = {
-                            heading: obj[i]["subject"],
-                            name: obj[i]["user_id"],
-                            level: obj[i]["level"],
-                            date: "Tue Apr 07 2015 13:32PM",
-                            post: obj[i]["post_id"],
-                            spaceName: obj[i]["module_id"],
-                            threadID: obj[i]["thread_id"],
-                            userID: obj[i]["user_id"],
-                            profilePick: "profile3.png",
-                            moduleID: obj[i]["module_id"]
-                        };
-                     }
-                    threads = JSON.stringify(threads);
-                    threads = JSON.parse(threads);
-                    res.render('threads', {title: 'D3', threads: threads, user: req.user, content: 'This is the module code for a thread'});
-                }
-        });
     });
+
+
 
     router.post('/spaces/:spaceName',function(req,res)
     {
         var subject=req.body.subject;
         var post=req.body.content;
-
-        var Thread = require('../models/thread');
-        //Check How Many Threads Are In The Database So You Can Form The ThreadID
-        var threadID = 0;
-        Thread.find(function (err, aThread)
+        var parent = null;
+        var module = req.params.spaceName;
+        try
         {
-            if (err)
-                console.log("ERR: " + err);
-            else
-            {
-                console.log("Found: " + JSON.stringify(aThread));
-                threadID = Number(Object.keys(aThread).length) + 1;
-                var len = Number(Object.keys(aThread).length);
-                console.log(len + " " + threadID);
-                var newThread = new Thread();
-
-                newThread.thread_id = threadID;
-                console.log(newThread.thread_id);
-                //This is a level 1 thread, so no parent (i.e. the parent is the current thread).
-                newThread.parent_thread_id = threadID;
-                newThread.user_id = "u12345678";
-                newThread.num_children = 0;
-                newThread.closed = false;
-                newThread.hidden = false;
-                newThread.level = "1";
-                newThread.post_id = post;
-                newThread.subject = subject;
-                newThread.module_id = req.params.spaceName;
-
-                //This is how you save it
-                newThread.save(function (err) {
-                    if (err) console.log("ERR: " + err);
-                    console.log("Saving: " + JSON.stringify(newThread));
-                });
-
-            }
-
-        });
+            createNewThread(subject, post, parent, module);
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
         res.end("done");
     });
 
     router.get('/spaces/:spaceName/:threadID', isLoggedIn, function (req, res) {
-        //Generate Threads from Database
-        var amount = 0;
-        var threads = new Array();
-        var Thread = require('../models/thread');
-        var module = req.params.spaceName;
-        var parent = req.params.threadID;
-
-        //Find the details of the thread we're exploring
-        Thread.find({'thread_id': parent}, function (err, validThread)
+        try
         {
-            if (err)
-                console.log("ERR: " + err);
-            else {
-                //Generate those threads
-                var stringifiedJSON = JSON.stringify(validThread);
-                console.log(stringifiedJSON);
-                var obj = JSON.parse(stringifiedJSON);
-                console.log(obj[0].level);
-                var parentLevel = 0;
-                parentLevel = obj[0].level + 1;
-                console.log("Level: " + parentLevel);
-                console.log("Module: " + module);
-                console.log("Parent: " + parent);
-                //Find the threads on level of the thread for this module whose parent is the thread we navigated to
-                Thread.find({'level' : parentLevel, 'module_id' : module, 'parent_thread_id' : parent }, function (err, validThread)
-                {
-
-                    if (err)
-                        console.log("ERR: " + err);
-                    else {
-                        //Generate those threads
-                        amount = Number(Object.keys(validThread).length);
-                        var stringifiedJSON = JSON.stringify(validThread);
-                        console.log(stringifiedJSON);
-                        var obj = JSON.parse(stringifiedJSON);
-                        for(var i = 0; i < amount; i++)
-                        {
-                            threads[i] = {
-                                heading: obj[i]["subject"],
-                                name: obj[i]["user_id"],
-                                level: obj[i]["level"],
-                                date: "Tue Apr 07 2015 13:32PM",
-                                post: obj[i]["post_id"],
-                                spaceName: obj[i]["module_id"],
-                                threadID: obj[i]["thread_id"],
-                                userID: obj[i]["user_id"],
-                                profilePick: "profile3.png",
-                                moduleID: obj[i]["module_id"]
-                            };
-                        }
-                        threads = JSON.stringify(threads);
-                        threads = JSON.parse(threads);
-                        res.render('threads', {title: 'D3', threads: threads, user: req.user, content: 'This is the module code for a thread'});
-                    }
-                });
-            }
-        });
+            var module = req.params.spaceName;
+            var parent = req.params.threadID;
+            getChildThreads(module, parent, req, res, threadCreateCallback);
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
     });
     router.post('/spaces/:spaceName/:threadID',function(req,res)
     {
         var subject=req.body.subject;
         var post=req.body.content;
-
-        var Thread = require('../models/thread');
-        //Check How Many Threads Are In The Database So You Can Form The ThreadID
-        var threadID = 0;
-        Thread.find(function (err, aThread) {
-            if (err)
-                console.log("ERR: " + err);
-            else
-            {
-                console.log("Found: " + JSON.stringify(aThread));
-                threadID = Number(Object.keys(aThread).length) + 1;
-            }
-
-        });
-        //The parent thread is the current thread
-        var parentThread = req.params.threadID;
-
-        //Get the parent thread's level
-        var parentLevel;
-        Thread.findOne({ 'thread_id' :  parentThread }, function (err, aThread) {
-            if (err)
-                console.log("ERR: " + err);
-            else
-            {
-                var stringifiedJSON = JSON.stringify(aThread);
-                var obj = JSON.parse(stringifiedJSON);
-                parentLevel = obj["level"];
-
-                var newThread = new Thread();
-
-                newThread.thread_id = threadID;
-                //This is a level 1 thread, so no parent.
-                newThread.parent_thread_id = parentThread;
-                newThread.user_id = "u12345678";
-                newThread.num_children = 0;
-                newThread.closed = false;
-                newThread.hidden = false;
-                newThread.level = Number(parentLevel)+1;
-                newThread.post_id = post;
-                newThread.subject = subject;
-                newThread.module_id = req.params.spaceName;
-
-                //This is how you save it
-                newThread.save(function (err) {
-                    if (err) console.log("ERR: " + err);
-                    console.log("Saving: " + JSON.stringify(newThread));
-                });
-            }
-        });
-
-
-
-
+        var parent = req.params.threadID;
+        var module = req.params.spaceName;
+        try
+        {
+            createNewThread(subject, post, parent, module);
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
         res.end("done");
     });
 
